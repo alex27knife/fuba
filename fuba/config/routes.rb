801e0bc4ab2626dab6dea52bfaba308ee6fae8b6
Andre Castoldi <avcastoldi@mac.com>
2013-09-02 20:39:21 -0400

    com bootstrap

--- fuba/config/routes.rb
+++ fuba/config/routes.rb
@@ -1,4 +1,14 @@
 Fuba::Application.routes.draw do
+  get "welcome/index"
+ 
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+  # ...
+  # You can have the root of your site routed with "root"
+  root to: "welcome#index"
+
+  resources :posts
+ 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
